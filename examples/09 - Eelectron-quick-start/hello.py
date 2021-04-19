308f85580a1e1bc55f74da63ee6c79782ded092b
Connor (work) <connor.taylor@icloud.com>
2021-04-19 16:22:09 +1200

    fix example

--- examples/09 - Eelectron-quick-start/hello.py
+++ examples/09 - Eelectron-quick-start/hello.py
@@ -10,11 +10,4 @@ def say_hello_py(x):
 say_hello_py('Python World!')
 eel.say_hello_js('Python World!')   # Call a Javascript function
 
-
-options = {
-	'mode': 'custom',
-	'args': ['node_modules/electron/dist/electron.exe', '.']
-}
-
-eel.start('hello.html', options=options)
-#eel.start('hello.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron.exe', '.'])
+eel.start('hello.html',mode='electron')
